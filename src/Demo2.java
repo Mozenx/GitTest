f62692c69b0cd46e7ba0d9db2f5e5fb61ba376bc
mohsen <mim_z@hotmail.com>
2019-04-16 12:48:13 +0430

    create object by Rectangle class

--- src/Demo2.java
+++ src/Demo2.java
@@ -1,5 +1,12 @@
 public class Demo2 {
     public static void main(String[] args) {
-        
+        Rectangle rec = new Rectangle(new Shape(){
+
+            @Override
+            public void getType(){
+                System.out.println("Rectagle");
+            }
+        });
+        rec.show();
     }
 }
