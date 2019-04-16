cb83d72e15d879dd76509d7c5ba5c18a9c3ac0ec
mohsen <mim_z@hotmail.com>
2019-04-16 11:09:33 +0430

    added main method, create anonymous class by Color interface, overriden getColor method of interface color

--- src/DemoColor.java
+++ src/DemoColor.java
@@ -1,2 +1,13 @@
 public class DemoColor {
+
+    public static void main(String[] args) {
+        Color c1 = new Color() {
+            String color = "red";
+            @Override
+            public void getColor() {
+                System.out.println(color);
+            }
+        };
+        c1.getColor();
+    }
 }
